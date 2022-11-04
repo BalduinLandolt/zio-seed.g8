ed2c72db9655807cda2d4ddf8df446b1c26e78a3
Balduin Landolt <balduin.landolt@hotmail.com>
2022-11-04 16:15:15 +0100

    test template variables

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,3 +1,9 @@
 class Stub {
   // got any helpful boilerplate for your users?
+  /*
+  name = $name$
+  description = $description$
+  scala3 = $scala3$ -> $if(scala3.truthy)$ yes $else$ no $endif$
+  scalatest_version = $scalatest_version$
+  */
 }
