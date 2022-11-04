e99e72553c698d451a1eabe528ce6c5c681ee476
Balduin Landolt <balduin.landolt@hotmail.com>
2022-11-04 16:18:54 +0100

    more testing

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,9 +1,6 @@
-class Stub {
+class $name$ {
   // got any helpful boilerplate for your users?
   /*
-  name = $name$
-  description = $description$
-  scala3 = $scala3$ -> $if(scala3.truthy)$ yes $else$ no $endif$
-  scalatest_version = $scalatest_version$
+  ...
   */
 }
